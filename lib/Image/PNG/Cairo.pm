7b83dec133a4883dc028a67a1089f4ecb1c5ff4b
Ben Bullock <bkb@cpan.org>
2014-02-04 13:10:17 +0000

    Import of BKB/Image-PNG-Cairo-0.05 from CPAN.
    gitpan-cpan-distribution: Image-PNG-Cairo
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         BKB/Image-PNG-Cairo-0.05.tar.gz
    gitpan-cpan-author:       BKB
    gitpan-cpan-maturity:     released

--- lib/Image/PNG/Cairo.pm
+++ lib/Image/PNG/Cairo.pm
@@ -1,40 +1,3 @@
-=encoding UTF-8
-
-=head1 NAME
-
-Image::PNG::Cairo - extract PNG data from a Cairo::ImageSurface
-
-=head1 SYNOPSIS
-
-    use Image::PNG::Cairo 'cairo_to_png';
-    use Cairo;
-    my $surface = Cairo::ImageSurface->new ('argb32', 100, 100);
-    # Draw something on surface.
-    my $png = cairo_to_png ($surface);
-    # Now can use the methods of Image::PNG::Libpng on the PNG,
-    # e.g. write to file.
-
-=head1 DESCRIPTION
-
-This is a bridge between L<Cairo> and L<Image::PNG::Libpng> which
-allows the user to extract the image data from a Cairo::ImageSurface
-into a structure which can then be manipulated to add other kinds of
-data.
-
-=head1 FUNCTIONS
-
-=head2 cairo_to_png
-
-    my $png = cairo_to_png ($surface);
-
-Only surfaces of type 'argb32' are supported.
-
-=head1 LICENCE, COPYRIGHT, AUTHOR
-
-Copyright Ben Bullock <bkb@cpan.org> 2014; this module may be used,
-redistributed, and modified under the same terms as Perl itself.
-
-=cut
 package Image::PNG::Cairo;
 require Exporter;
 @ISA = qw(Exporter);
@@ -45,7 +8,7 @@ require Exporter;
 use warnings;
 use strict;
 use Carp;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 require XSLoader;
 XSLoader::load ('Image::PNG::Cairo', $VERSION);
 use Cairo;
